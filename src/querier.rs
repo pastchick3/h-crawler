87941e58e1ababd8f0f19be6e72825ce188800dd
pastchick3 <331604390@qq.com>
2020-05-17 19:40:29 -0400

    init dev

--- src/querier.rs
+++ src/querier.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::time::Duration;
 
 use reqwest::Client;
+use scraper::Html;
 
 const USER_AGENT: &str = concat!(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ",
@@ -33,6 +34,52 @@ impl From<Category> for String {
     fn from(category: Category) -> Self {
         category.bits.to_string()
     }
+}
+
+// {
+//     "gid": 618395,
+//     "token": "0439fa3666",
+//     "archiver_key": "403565--d887c6dfe8aae79ed0071551aa1bafeb4a5ee361",
+//     "title": "(Kouroumu 8) [Handful☆Happiness! (Fuyuki Nanahara)] TOUHOU GUNMANIA A2 (Touhou Project)",
+//     "title_jpn": "(紅楼夢8) [Handful☆Happiness! (七原冬雪)] TOUHOU GUNMANIA A2 (東方Project)",
+//     "category": "Non-H",
+//     "thumb": "https://ehgt.org/14/63/1463dfbc16847c9ebef92c46a90e21ca881b2a12-1729712-4271-6032-jpg_l.jpg",
+//     "uploader": "avexotsukaai",
+//     "posted": "1376143500",
+//     "filecount": "20",
+//     "filesize": 51210504,
+//     "expunged": false,
+//     "rating": "4.43",
+//     "torrentcount": "0",
+//     "tags": [
+//         "parody:touhou project",
+//         "group:handful happiness",
+//         "artist:nanahara fuyuki",
+//         "full color",
+//         "artbook"
+//     ]
+// }
+pub struct EHResponse {
+    gid: u64,
+    token: String,
+    archiver_key: String,
+    title: String,
+    title_jpn: String,
+    category: String,
+    thumb: String,
+    uploader: String,
+    posted: String,
+    filecount: String,
+    filesize: u64,
+    expunged: bool,
+    rating: String,
+    torrentcount: String,
+    tags: Vec<String>,
+}
+
+pub struct QueryResult {
+    count: u64,
+    galleries: Vec<EHResponse>,
 }
 
 pub struct Querier {
@@ -87,17 +134,92 @@ impl Querier {
     }
 
     /// {
-    ///     "Category": [String],
-    ///     "Search": [String],
-    ///     tag: [String],
+    ///     "Category": Vec<String>,
+    ///     "Search": Vec<String>,
+    ///     tag: Vec<String>,
     /// }
-    pub async fn query(&self, params: HashMap<String, String>) -> HashMap<String, String> {
-        // toggle_category
-        HashMap::new()
-        //
+    pub async fn query(
+        &self,
+        exclusive: bool,
+        params: HashMap<String, Vec<String>>,
+    ) -> Result<QueryResult, String> {
+        let mut category = String::from("0");
+        let mut query_string = String::new();
+        for (key, value) in params.iter() {
+            match key.to_lowercase().as_str() {
+                "category" => {
+                    category = self.toggle_category(value)?;
+                }
+                "search" => {
+                    for term in value.iter() {
+                        let segment = format!("+{:?}", term);
+                        query_string.push_str(&segment);
+                    }
+                }
+                tag => {
+                    for term in value.iter() {
+                        let segment = format!("+{}:{:?}$", tag, term);
+                        query_string.push_str(&segment);
+                    }
+                }
+            }
+        }
+        let page = self
+            .client
+            .get(EX_URL)
+            .query(&[("page", 0u8)])
+            .query(&[("f_cats", &category)])
+            .query(&[("f_search", &query_string)])
+            .send()
+            .await
+            //.log_err()
+            .map_err(|_| String::from("Unable to load the first page."))?
+            .text()
+            .await
+            .unwrap();
+        let (page_count, mut galleries) = self.parse_page(&page);
+        for pg in 1..page_count {
+            let (_, g) = self.parse_page(&page);
+            self.client
+                .get(EX_URL)
+                .query(&[("page", pg)])
+                .query(&[("f_cats", &category)])
+                .query(&[("f_search", &query_string)])
+                .send()
+                .await
+                //.log_err()
+                .map_err(|_| String::from("Unable to load the first page."))?;
+            galleries.extend(g);
+        }
+
+        self.query_api(galleries)
     }
 
-    fn toggle_category(&self, categories: &[String])-> Result<String, String> {
+    fn parse_page(&self, page: &str) -> (u64, Vec<(u64, u64)>) {
+        let document = Html::parse_document(page);
+        todo!()
+    }
+
+    fn query_api(&self, galleries: Vec<(u64, u64)>) -> Result<QueryResult, String> {
+        todo!()
+    }
+
+    // function toggle_category(b) {
+    //     // 每关一个就 | 对应的值
+    //     var a = document.getElementById("f_cats"); // init 0
+    //     var c = document.getElementById("cat_" + b);
+    //     if (a.getAttribute("disabled")) {
+    //         a.removeAttribute("disabled")
+    //     }
+    //     if (c.getAttribute("data-disabled")) {
+    //         c.removeAttribute("data-disabled");
+    //         a.value = parseInt(a.value) & (1023 ^ b)
+    //     } else {
+    //         c.setAttribute("data-disabled", 1);
+    //         a.value = parseInt(a.value) | b
+    //     }
+    // }
+    fn toggle_category(&self, categories: &[String]) -> Result<String, String> {
         let mut category: Category = Default::default();
         for cat in categories {
             match cat.to_lowercase().as_str() {
@@ -111,25 +233,9 @@ impl Querier {
                 "cosplay" => category |= Category::COSPLAY,
                 "asian porn" => category |= Category::ASIAN_PORN,
                 "misc" => category |= Category::MISC,
-                _ => {return Err(String::from(cat))}
+                _ => return Err(String::from(cat)),
             }
         }
-        Ok(String::from(category))
+        Ok(String::from(!category))
     }
 }
-
-// function toggle_category(b) {
-//     // 每关一个就 | 对应的值
-//     var a = document.getElementById("f_cats"); // init 0
-//     var c = document.getElementById("cat_" + b);
-//     if (a.getAttribute("disabled")) {
-//         a.removeAttribute("disabled")
-//     }
-//     if (c.getAttribute("data-disabled")) {
-//         c.removeAttribute("data-disabled");
-//         a.value = parseInt(a.value) & (1023 ^ b)
-//     } else {
-//         c.setAttribute("data-disabled", 1);
-//         a.value = parseInt(a.value) | b
-//     }
-// }
