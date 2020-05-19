2bf9f6407c35a6cf4657733c30cef7cfab90f265
pastchick3 <331604390@qq.com>
2020-05-18 20:49:58 -0400

    init dev

--- src/querier.rs
+++ src/querier.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 use std::time::Duration;
 
+use regex::Regex;
 use reqwest::Client;
-use scraper::Html;
+use scraper::{Html, Selector};
+use serde::{Deserialize, Serialize};
+use tokio::time;
 
 const USER_AGENT: &str = concat!(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ",
@@ -12,7 +15,14 @@ const USER_AGENT: &str = concat!(
 );
 const EH_URL: &str = "https://e-hentai.org/";
 const EX_URL: &str = "https://exhentai.org/";
+const API_URL: &str = "https://api.e-hentai.org/api.php";
 const TIMEOUT: u64 = 30;
+const INTERVAL: u64 = 2;
+const GALLERIES_PER_PAGE: usize = 25;
+const GALLERY_COUNT_SELECTOR: &str = "div.ido div:nth-child(2) p";
+const GALLERY_COUNT_REGEX: &str = r"(\d|,)+";
+const GALLERY_SELECTOR: &str = "div.ido table.itg.gltc td.gl3c.glname a";
+const GALLERY_REGEX: &str = r"/([^/]+)/([^/]+)/$";
 
 bitflags! {
     #[derive(Default)]
@@ -36,30 +46,33 @@ impl From<Category> for String {
     }
 }
 
-// {
-//     "gid": 618395,
-//     "token": "0439fa3666",
-//     "archiver_key": "403565--d887c6dfe8aae79ed0071551aa1bafeb4a5ee361",
-//     "title": "(Kouroumu 8) [Handful☆Happiness! (Fuyuki Nanahara)] TOUHOU GUNMANIA A2 (Touhou Project)",
-//     "title_jpn": "(紅楼夢8) [Handful☆Happiness! (七原冬雪)] TOUHOU GUNMANIA A2 (東方Project)",
-//     "category": "Non-H",
-//     "thumb": "https://ehgt.org/14/63/1463dfbc16847c9ebef92c46a90e21ca881b2a12-1729712-4271-6032-jpg_l.jpg",
-//     "uploader": "avexotsukaai",
-//     "posted": "1376143500",
-//     "filecount": "20",
-//     "filesize": 51210504,
-//     "expunged": false,
-//     "rating": "4.43",
-//     "torrentcount": "0",
-//     "tags": [
-//         "parody:touhou project",
-//         "group:handful happiness",
-//         "artist:nanahara fuyuki",
-//         "full color",
-//         "artbook"
-//     ]
-// }
-pub struct EHResponse {
+type QueryResult<T> = Result<T, String>;
+type Gallery = (u64, String);
+
+#[derive(Serialize)]
+struct ApiRequest {
+    method: String,
+    gidlist: Vec<Gallery>,
+    namespace: u64,
+}
+
+impl ApiRequest {
+    fn new<T: Iterator<Item = Gallery>>(galleries: T) -> Self {
+        ApiRequest {
+            method: String::from("gdata"),
+            gidlist: galleries.collect(),
+            namespace: 1,
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct ApiResponse {
+    gmetadata: Vec<Meta>,
+}
+
+#[derive(Deserialize)]
+pub struct Meta {
     gid: u64,
     token: String,
     archiver_key: String,
@@ -77,16 +90,21 @@ pub struct EHResponse {
     tags: Vec<String>,
 }
 
-pub struct QueryResult {
-    count: u64,
-    galleries: Vec<EHResponse>,
+pub struct GalleryPack {
+    count: usize,
+    data: Vec<Meta>,
 }
 
 pub struct Querier {
     client: Client,
+    ex: bool,
+    url: &'static str,
     username: Option<String>,
     password: Option<String>,
-    ex: bool,
+    gallery_count_selector: Selector,
+    gallery_count_regex: Regex,
+    gallery_selector: Selector,
+    gallery_regex: Regex,
 }
 
 impl Querier {
@@ -99,9 +117,14 @@ impl Querier {
             .expect("Unable to initialize the request client.");
         Querier {
             client,
+            ex: false,
+            url: EH_URL,
             username: None,
             password: None,
-            ex: false,
+            gallery_count_selector: Selector::parse(GALLERY_COUNT_SELECTOR).unwrap(),
+            gallery_count_regex: Regex::new(GALLERY_COUNT_REGEX).unwrap(),
+            gallery_selector: Selector::parse(GALLERY_SELECTOR).unwrap(),
+            gallery_regex: Regex::new(GALLERY_REGEX).unwrap(),
         }
     }
 
@@ -127,99 +150,58 @@ impl Querier {
 
         Querier {
             client,
+            ex: true,
+            url: EX_URL,
             username: Some(username),
             password: Some(password),
-            ex: true,
+            gallery_count_selector: Selector::parse(GALLERY_COUNT_SELECTOR).unwrap(),
+            gallery_count_regex: Regex::new(GALLERY_COUNT_REGEX).unwrap(),
+            gallery_selector: Selector::parse(GALLERY_SELECTOR).unwrap(),
+            gallery_regex: Regex::new(GALLERY_REGEX).unwrap(),
         }
     }
 
-    /// {
-    ///     "Category": Vec<String>,
-    ///     "Search": Vec<String>,
-    ///     tag: Vec<String>,
-    /// }
     pub async fn query(
         &self,
-        exclusive: bool,
+        exhaustive: bool,
         params: HashMap<String, Vec<String>>,
-    ) -> Result<QueryResult, String> {
+    ) -> QueryResult<GalleryPack> {
         let mut category = String::from("0");
-        let mut query_string = String::new();
+        let mut search = String::new();
         for (key, value) in params.iter() {
             match key.to_lowercase().as_str() {
                 "category" => {
                     category = self.toggle_category(value)?;
                 }
                 "search" => {
-                    for term in value.iter() {
+                    for term in value {
                         let segment = format!("+{:?}", term);
-                        query_string.push_str(&segment);
+                        search.push_str(&segment);
                     }
                 }
                 tag => {
-                    for term in value.iter() {
+                    for term in value {
                         let segment = format!("+{}:{:?}$", tag, term);
-                        query_string.push_str(&segment);
+                        search.push_str(&segment);
                     }
                 }
             }
         }
-        let page = self
-            .client
-            .get(EX_URL)
-            .query(&[("page", 0u8)])
-            .query(&[("f_cats", &category)])
-            .query(&[("f_search", &query_string)])
-            .send()
-            .await
-            //.log_err()
-            .map_err(|_| String::from("Unable to load the first page."))?
-            .text()
-            .await
-            .unwrap();
-        let (page_count, mut galleries) = self.parse_page(&page);
-        for pg in 1..page_count {
-            let (_, g) = self.parse_page(&page);
-            self.client
-                .get(EX_URL)
-                .query(&[("page", pg)])
-                .query(&[("f_cats", &category)])
-                .query(&[("f_search", &query_string)])
-                .send()
-                .await
-                //.log_err()
-                .map_err(|_| String::from("Unable to load the first page."))?;
-            galleries.extend(g);
+
+        let page = self.request_page(0, &category, &search).await?;
+        let (page_count, mut galleries) = self.parse_page(&page)?;
+        if exhaustive {
+            for pg in 1..page_count {
+                let page = self.request_page(pg, &category, &search).await?;
+                let (_, g) = self.parse_page(&page)?;
+                galleries.extend(g);
+            }
         }
 
-        self.query_api(galleries)
+        self.request_meta(galleries).await
     }
 
-    fn parse_page(&self, page: &str) -> (u64, Vec<(u64, u64)>) {
-        let document = Html::parse_document(page);
-        todo!()
-    }
-
-    fn query_api(&self, galleries: Vec<(u64, u64)>) -> Result<QueryResult, String> {
-        todo!()
-    }
-
-    // function toggle_category(b) {
-    //     // 每关一个就 | 对应的值
-    //     var a = document.getElementById("f_cats"); // init 0
-    //     var c = document.getElementById("cat_" + b);
-    //     if (a.getAttribute("disabled")) {
-    //         a.removeAttribute("disabled")
-    //     }
-    //     if (c.getAttribute("data-disabled")) {
-    //         c.removeAttribute("data-disabled");
-    //         a.value = parseInt(a.value) & (1023 ^ b)
-    //     } else {
-    //         c.setAttribute("data-disabled", 1);
-    //         a.value = parseInt(a.value) | b
-    //     }
-    // }
-    fn toggle_category(&self, categories: &[String]) -> Result<String, String> {
+    fn toggle_category(&self, categories: &[String]) -> QueryResult<String> {
         let mut category: Category = Default::default();
         for cat in categories {
             match cat.to_lowercase().as_str() {
@@ -233,9 +215,103 @@ impl Querier {
                 "cosplay" => category |= Category::COSPLAY,
                 "asian porn" => category |= Category::ASIAN_PORN,
                 "misc" => category |= Category::MISC,
-                _ => return Err(String::from(cat)),
+                c => return Err(format!("Invalid category: {}", c)),
             }
         }
         Ok(String::from(!category))
+    }
+
+    async fn request_page(
+        &self,
+        page: u64,
+        category: &str,
+        search: &str,
+    ) -> Result<String, String> {
+        self.client
+            .get(self.url)
+            .query(&[("page", page)])
+            .query(&[("f_cats", category)])
+            .query(&[("f_search", search)])
+            .send()
+            .await
+            .map_err(|err| format!("{}", err))?
+            .text()
+            .await
+            .map_err(|err| format!("{}", err))
+    }
+
+    fn parse_page(&self, page: &str) -> QueryResult<(u64, Vec<Gallery>)> {
+        let document = Html::parse_document(page);
+
+        let gallery_count_str = document
+            .select(&self.gallery_count_selector)
+            .next()
+            .ok_or(String::from("Unable to locate the gallery count."))?
+            .inner_html();
+        let gallery_count = self
+            .gallery_count_regex
+            .captures(&gallery_count_str)
+            .ok_or(String::from("Unable to extract the gallery count."))?
+            .get(0)
+            .ok_or(String::from("Unable to extract the gallery count."))?
+            .as_str()
+            .replace(",", "")
+            .parse::<u64>()
+            .map_err(|_| String::from("Unable to extract the gallery count."))?;
+        let page_count = (gallery_count as f64 / GALLERIES_PER_PAGE as f64).ceil() as u64;
+
+        let gallery_links = document.select(&self.gallery_selector).collect::<Vec<_>>();
+        if gallery_links.len() != GALLERIES_PER_PAGE {
+            return Err(String::from("Unable to locate galleries."));
+        }
+        let mut galleries = Vec::new();
+        for link in gallery_links {
+            let href = link
+                .value()
+                .attr("href")
+                .ok_or("Unable to extract the gallery link.")?;
+            let caps = self
+                .gallery_regex
+                .captures(&href)
+                .ok_or("Unable to extract the gallery link.")?;
+            let id = caps
+                .get(1)
+                .ok_or("Unable to extract the gallery link.")?
+                .as_str()
+                .parse()
+                .map_err(|_| String::from("Unable to extract the gallery link."))?;
+            let token = caps
+                .get(2)
+                .ok_or("Unable to extract the gallery link.")?
+                .as_str()
+                .to_string();
+            galleries.push((id, token));
+        }
+
+        Ok((page_count, galleries))
+    }
+
+    async fn request_meta(&self, galleries: Vec<Gallery>) -> QueryResult<GalleryPack> {
+        let mut pack = GalleryPack {
+            count: galleries.len(),
+            data: Vec::new(),
+        };
+        for chunk in galleries.chunks(GALLERIES_PER_PAGE) {
+            let params = ApiRequest::new(chunk.iter().map(|g| g.clone()));
+            let meta = self
+                .client
+                .post(API_URL)
+                .json(&params)
+                .send()
+                .await
+                .map_err(|_| String::from("Unable to request gallery metadata."))?
+                .json::<ApiResponse>()
+                .await
+                .map_err(|_| String::from("Unable to request gallery metadata."))?
+                .gmetadata;
+            pack.data.extend(meta);
+            time::delay_for(Duration::from_secs(INTERVAL)).await;
+        }
+        Ok(pack)
     }
 }
